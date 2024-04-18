f49050b34c837f7ffbb1231a3353f67f7a534310
Thorsten Kober <tkober@gmail.com>
2024-04-17 20:03:37 -0400

    rebuild

--- src/components/masonry-layout/masonry-layout.jsx
+++ src/components/masonry-layout/masonry-layout.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-
 import ImageDisplay from "../image-display/image-display";
 import styles from "./masonry-layout.module.css";
+
+import { useResizeObserver } from "@/hooks/use-resize-observer/use-resize-observer";
+import { useThrottle } from "@/hooks/use-throttle/use-throttle";
+
+export const getNewHeight = (width, height, targetWidth) =>
+  (height / width) * targetWidth;
 
 export default function MasonryLayout({
     data = { items: [] },
@@ -31,13 +36,49 @@ export default function MasonryLayout({
         height: "auto"
     }
 
+    const [sizes, setSizes] = useState(
+        data.items.map(() => ({
+            width: 0,
+            height: 0,
+        }))
+    );
+    const [containerWidth, setContainerWidth] = useState(-1);
+    const { elementRef, disconnect } = useResizeObserver({
+        callback: useThrottle(handleOnResize, 0),
+    });
+
+    function handleOnResize(entries) {
+        for (let entry of entries) {
+            if (containerWidth === entry.contentRect.width) {
+                return;
+            }
+
+            disconnect();
+            setContainerWidth(entry.contentRect.width);
+            resize(entry.contentRect.width);
+        }
+    }
+
+    function resize(containerWidth) {
+        console.log("resize", containerWidth);
+        const newWidth = containerWidth / numColumns;
+        const newSizes = data.items.map((entry) => {
+            const item = { ...entry.image };
+            item.height = getNewHeight(item.width, item.height, newWidth);
+            item.width = newWidth;
+            return item;
+        });
+        
+        setSizes(newSizes);
+    }
+
     return (
-        <div className={styles["masonry-container"]}>
+        <div className={styles["masonry-container"]}ref={elementRef}>
             <div className={styles["masonry-content"]}>
                 {columns.map((column, index) => {
                     return (
                         <div key={`masonry-column-${index}`} className={styles["masonry-column"]}>
-                         {column.map((item) => <ImageDisplay key={item.id} data={item} width={item.image.width} height={item.image.height} containerStyles={customStyles}/>)}
+                         {column.map((item) => <ImageDisplay key={item.id} data={item} width={sizes[index].width} height={sizes[index].height} containerStyles={customStyles}/>)}
                         </div>
                     )
                 })}
